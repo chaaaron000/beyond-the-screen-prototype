bd814940620edae3c99cb53fb4b8657bff8a0f99
Vandal <bjpowerll12967@gmail.com>
2026-08-26 18:03:14 +0900

    fix: normalize report typography

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import "./styles/themes.css";
+import "./styles/typography.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
